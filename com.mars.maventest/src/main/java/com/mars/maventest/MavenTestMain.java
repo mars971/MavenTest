419078495fdfdc462ffd410bbb4e12cae5da8ac8
Mars <maxence.tissot@live.fr>
2016-09-07 00:01:21 +0200

    My first new branch

--- com.mars.maventest/src/main/java/com/mars/maventest/MavenTestMain.java
+++ com.mars.maventest/src/main/java/com/mars/maventest/MavenTestMain.java
@@ -5,6 +5,10 @@ public class MavenTestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
+User myFirstuser = new User();
+myFirstuser.setName("toto");
+
+System.out.println(myFirstuser.getName());
 	}
 
 }

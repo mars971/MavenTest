92ad4f2a7bbb521a8348a5992cd90bae6b83633f
Mars <maxence.tissot@live.fr>
2016-09-06 23:53:35 +0200

    creation maven test project with github

---
+++ com.mars.maventest/src/main/java/com/mars/maventest/MavenTestMain.java
@@ -0,0 +1,10 @@
+package com.mars.maventest;
+
+public class MavenTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
